45925287b761e0f6c8c4663988a0928b80a20d30
Patryk <patrykdepka@gmail.com>
2023-04-08 16:36:14 +0200

    App generated by start.spring.io

---
+++ src/test/java/pl/patrykdepka/iteventsapi/ItEventsApiTests.java
@@ -0,0 +1,13 @@
+package pl.patrykdepka.iteventsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItEventsApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
